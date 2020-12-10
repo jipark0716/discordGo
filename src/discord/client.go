45feba6bdd7314643c05be39a6348508ecdcc872
jipark0716 <jipark0716@gmail.com>
2020-12-10 19:42:06 +0900

    bug fix

--- src/discord/client.go
+++ src/discord/client.go
@@ -34,25 +34,27 @@ func Connect(token string) Client {
     socket.OnTextMessage = this.EventAdapter.onTextMessageEvent
     socket.OnConnected = func(socket gowebsocket.Socket) {
         log.Println("Connected to server");
-    };
+    }
     socket.OnConnectError = func(err error, socket gowebsocket.Socket) {
         log.Println("Recieved connect error ", err)
-    };
+    }
     socket.OnBinaryMessage = func(data [] byte, socket gowebsocket.Socket) {
         log.Println("Recieved binary data ", data)
-    };
+    }
     socket.OnPingReceived = func(data string, socket gowebsocket.Socket) {
         log.Println("Recieved ping " + data)
-    };
+    }
     socket.OnPongReceived = func(data string, socket gowebsocket.Socket) {
         log.Println("Recieved pong " + data)
-    };
-    socket.OnDisconnected = func(err error, socket gowebsocket.Socket) {
-        log.Println("Disconnected from server ")
-        return
-    };
+    }
+    socket.OnDisconnected = this.reconnect
     socket.Connect()
     return this
+}
+
+func (this *Client) reconnect(err error, socket gowebsocket.Socket) {
+    log.Println("Disconnected from server ")
+    socket.Connect()
 }
 
 func SendJson(data map[string]interface{}, socket gowebsocket.Socket) {
